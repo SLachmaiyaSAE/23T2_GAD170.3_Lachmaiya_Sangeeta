e07f7be17d9b94058e1da8494ce15821fd1794da
SLachmaiyaSAE <136556179+SLachmaiyaSAE@users.noreply.github.com>
2023-08-11 16:17:33 +1000

    Added impact tester script to knives object and trying to respawn it on destroy.
    writing script stuff. As always a wee bit of a clunky experience. But getting there. Next up is my spawning of text / buttons :P

--- Assets/Scripts/ImpactTester.cs
+++ Assets/Scripts/ImpactTester.cs
@@ -1,41 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.VFX;
+
+
 
 public class ImpactTester : MonoBehaviour
 {
-    // we want this script to handle COLLISION and TRIGGER detection
-
-    // when our object impacts another, we want to PRINT TEXT
-
-    // if you put UnityEngine. 
-
-
-    // collision enter 
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Ouch");
-
-        // play a sound effect
-        // destroy one or two other objects
-        // change colour of object
-        // Show a particle effect
-
-        // Destroy the a game object that this cript is attached to
-        Destroy(gameObject);
-
-        // 
-        Destroy(collision.gameObject);
-
-    }
+    public GameObject KnifeKitchenPrefab;
+    public Vector3 spawnPosition;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Ouch, i have triggered something");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("You've been chopped up");
+        }
+        else if (other.gameObject.CompareTag("Untagged"))
+        {
+            DestroySelf();
+            Instantiate(KnifeKitchenPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 
-
-
+    private void DestroySelf()
+    {
+        // Implement your destroy logic here
+        Destroy(gameObject);
+    }
 }
+
+// ORIGINAL CODE GIVEN TO US IN CLASS FROM AARON
+
+//public class ImpactTester : MonoBehaviour
+//{
+//    // we want this script to handle COLLISION and TRIGGER detection
+//    // when our object impacts another, we want to PRINT TEXT
+//    // if you put UnityEngine. 
+//    // collision enter 
+//    private void OnCollisionEnter(Collision collision)
+//    {
+//        Debug.Log("Ouch");
+//        // play a sound effect
+//        // destroy one or two other objects
+//        // change colour of object
+//        // Show a particle effect
+//        // Destroy a game object that this script is attached to
+//        Destroy(gameObject);
+//        // 
+//        Destroy(collision.gameObject);
+//    }
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        Debug.Log("Ouch, i have triggered something");
+//    }
+
---
+++ Assets/Scripts/KnifeSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeSpawner : MonoBehaviour
+{
+    public GameObject KnifeKitchenPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

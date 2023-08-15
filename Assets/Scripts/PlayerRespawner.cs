321d3ddb81ebd8558e4d2b742a9c6ca640159f66
SLachmaiyaSAE <136556179+SLachmaiyaSAE@users.noreply.github.com>
2023-08-15 16:23:12 +0300

    Adding player Respawner and TextMeshPro
    buttons, and instructions

--- Assets/Scripts/PlayerRespawner.cs
+++ Assets/Scripts/PlayerRespawner.cs
@@ -1,26 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerRespawner : MonoBehaviour
 {
+    [SerializeField] GameObject playerCharacter;
+    [SerializeField] CharacterController controller;
+    [SerializeField] TextMeshProUGUI tankReturnMessage;
 
-    public float threshold = 0;
-    public float PlayerRespawn = 0f;
-    public GameObject octopus;
-
-    void DestroySelf()
+    private void OnTriggerEnter(Collider other)
     {
-        if(transform.position.y < threshold)
+        if (other.CompareTag("Enemy"))
         {
-            DestroySelf();
-            Vector3 spawnPosition = transform.position + new Vector3(0f, PlayerRespawn, 0f);
-            Instantiate(octopus, spawnPosition, Quaternion.identity);
+            ShowTankReturnMessage();
+            Respawn();
         }
-            
+    }
+
+    private void ShowTankReturnMessage()
+    {
+        tankReturnMessage.gameObject.SetActive(true);
+        tankReturnMessage.text = "The head chef caught you! Returned to tank";
+    }
+
+    private void Respawn()
+    {
+        controller.enabled = false;
+        Vector3 teleportPosition = new Vector3(-56.6f, 9.2f, -20.7f);
+        playerCharacter.transform.position = teleportPosition;
+        controller.enabled = true;
     }
 }
 
+
+
+
+
+
+
+//    public float threshold = 0;
+//    public float PlayerRespawn = 0f;
+//    public GameObject octopus;
+
+//    void DestroySelf()
+//    {
+//        if(transform.position.y < threshold)
+//        {
+//            DestroySelf();
+//            Vector3 spawnPosition = transform.position + new Vector3(0f, PlayerRespawn, 0f);
+//            Instantiate(octopus, spawnPosition, Quaternion.identity);
+//        }
+
+//    }
+//}
 
 
 
